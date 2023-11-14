aa58a12620594550db3df20fef40b09a5406a490
DiazFarindra <farindrad@gmail.com>
2023-11-14 18:20:29 +0700

    fix

--- semester_1/dasar-dasar_pemrograman/praktikum_uts/kite.py
+++ semester_1/dasar-dasar_pemrograman/praktikum_uts/kite.py
@@ -1,5 +1,11 @@
-def kite(d1: int, d2: int):
+def kite_area(d1: int, d2: int):
     result = 0.5 * d1 * d2
+
+    return result
+
+
+def kite_circumference(a: int, b: int):
+    result = 2 * (a + b)
 
     return result
 
@@ -7,9 +13,18 @@ def kite(d1: int, d2: int):
 command = ''
 
 while command.lower() != 'no':
-    d1 = int(input('masukan diagonal pertama : '))
-    d2 = int(input('masukan diagonal kedua : '))
+    what = input('ingin menghitung layang-layang (luas/keliling) : ')
 
-    print(f'luas layang-layang : {kite(d1, d2)}')
+    if what.lower() == 'luas':
+        d1 = int(input('masukan diagonal pertama : '))
+        d2 = int(input('masukan diagonal kedua : '))
+
+        print(f'luas layang-layang : {kite_area(d1, d2)}')
+
+    if what.lower() == 'keliling':
+        a = int(input('masukan sisi atas : '))
+        b = int(input('masukan sisi bawah : '))
+
+        print(f'luas layang-layang : {kite_circumference(a, b)}')
 
     command = input('ingin menghitung lagi? (yes/no) : ')
