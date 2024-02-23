8fd37aa6b6fd3cc9812cbc444993e37cc6313a0b
diaz farindra <farindrad@gmail.com>
2024-02-23 12:11:14 +0700

    semester 2

---
+++ semester_2/pemrograman_web/praktikum01/variable.php
@@ -0,0 +1,20 @@
+<?php
+
+(string) $nama = 'Diaz Farindra';
+(string) $jurusan = 'Teknik Informatika';
+(int) $angkatan = 2023;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?= $nama; ?></h1>
+    <p>saya kuliah di jurusan <?= $jurusan; ?></p>
+</body>
+</html>
