48d4fa0ed110445789e2828fd8f3511e340241a5
diaz farindra <farindrad@gmail.com>
2024-10-25 15:01:38 +0700

    backend praktikum 05

---
+++ semester_3/backend/praktikum05/app/Http/Controllers/StudentController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StudentRequest;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $students = Student::get();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'List Data Student',
+            'students' => $students,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StudentRequest $request)
+    {
+        $data = $request->validated();
+
+        $student = Student::create($data);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Student Created',
+            'student' => $student,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(StudentRequest $request, Student $student)
+    {
+        $data = $request->validated();
+
+        $student->update($data);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Student Updated',
+            'student' => $student,
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Student $student)
+    {
+        $student->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Student Deleted',
+        ]);
+    }
+}
---
+++ semester_3/backend/praktikum05/routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\StudentController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/students', [StudentController::class, 'index']);
+Route::post('/students', [StudentController::class, 'store']);
+Route::get('/students/{student}', [StudentController::class, 'show']);
+Route::put('/students/{student}', [StudentController::class, 'update']);
+Route::delete('/students/{student}', [StudentController::class, 'destroy']);
