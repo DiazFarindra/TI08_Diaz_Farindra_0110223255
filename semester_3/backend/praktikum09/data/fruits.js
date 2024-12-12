915a05aab2853ba8ed13a5fbc8de49820c04ac7b
diaz farindra <farindrad@gmail.com>
2024-12-12 15:36:43 +0700

    praktikum 09

---
+++ semester_3/backend/praktikum09/data/fruits.js
@@ -0,0 +1,15 @@
+
+const fruits = [
+    'Apple',
+    'Banana',
+    'Cherry',
+    'Durian',
+    'Elderberry',
+    'Fig',
+    'Grape',
+    'Honeydew',
+    'Jackfruit',
+    'Kiwi',
+]
+
+export { fruits };
