d2da0ff15f9cddf4cf2da04d83e69618fc65f0b4
DiazFarindra <farindrad@gmail.com>
2025-02-28 02:02:31 +0700

    rearange semester 4 task folder

--- semester_1/dasar-dasar_pemrograman/praktikum06/tugas2.py
+++
@@ -1,8 +0,0 @@
-count = 0
-result = 0
-for index in range(1, 20, 2):
-    count = index
-    result += index
-    print(f'{count} + ', end='')
-
-print(f'= {result}')
--- semester_1/pemrograman_web/praktikum12/chat.js
+++
@@ -1,9 +0,0 @@
-
-function chat() {
-    const p = document.createElement('p')
-    p.appendChild(document.createTextNode(document.getElementById('text').value))
-
-    document.getElementById('chat').appendChild(p)
-
-    document.getElementById('text').value = ''
-}
