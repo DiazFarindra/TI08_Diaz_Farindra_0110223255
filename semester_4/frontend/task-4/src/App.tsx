4eec6033a5ec22719322ee7b66c3ffd92a057a23
DiazFarindra <farindrad@gmail.com>
2025-03-17 12:40:50 +0700

    task 4

---
+++ semester_4/frontend/task-4/src/App.tsx
@@ -0,0 +1,12 @@
+import Hello from "./components/Hello";
+
+const App = () => {
+  return (
+    <div className='p-10'>
+      <h1 className='text-4xl font-bold'>Hello World!</h1>
+      <Hello name='diaz' />
+    </div>
+  );
+}
+
+export default App;
---
+++ semester_4/frontend/task-4/src/components/Hello.tsx
@@ -0,0 +1,9 @@
+const Hello = ({ name }: { name: string }) => {
+    return (
+        <div>
+            <p>I'm {name} - Software Engineer</p>
+        </div>
+    );
+}
+
+export default Hello;
